7a3a991fbe6fe78e9fe8c0174d72f576656dacd0
keyhansa <keyhan.ahmad@gmail.com>
2025-03-24 13:50:27 +0330

    exported authOptions from another location

--- src/app/api/delete-image/route.ts
+++ src/app/api/delete-image/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
 import cloudinary from "@/lib/cloudinary-server"
 import { getServerSession } from "next-auth/next"
-import { authOptions } from "../auth/[...nextauth]/route"
+import { authOptions } from "@/app/utils/authOtions"
 
 export async function POST(request: Request) {
   try {
