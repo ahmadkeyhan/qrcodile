defae7b5fc708ec16eb99e3fa70f891330d1789f
keyhansa <keyhan.ahmad@gmail.com>
2025-03-29 19:41:37 +0330

    responsice image for product card

--- src/components/roastery/productCard.tsx
+++ src/components/roastery/productCard.tsx
@@ -7,14 +7,16 @@ export default function ProductCard({product}: {product:IProduct}) {
     if (!product) return null
     return (
         <div className="flex flex-col">
-            <Image 
-                src={product.image || "/placeholder.svg?height=250&width=500"}
-                alt={product.name}
-                className="object-cover"
-                width={100}
-                height={100}
-                priority 
-            />
+            <div className="w-full">
+                <Image 
+                    src={product.image || "/placeholder.svg?height=250&width=500"}
+                    alt={product.name}
+                    width={300}
+                    height={300}
+                    style={{width: '100%', height: 'auto'}}
+                    priority 
+                />
+            </div>
             <div className="flex flex-row-reverse justify-between items-center">
                 <p>{product.name}</p>
                 <p>{product.price}</p>
